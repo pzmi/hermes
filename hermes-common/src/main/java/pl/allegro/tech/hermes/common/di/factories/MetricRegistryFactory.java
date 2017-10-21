13c4039bb15f2c6ad909531411f5a7fb40c7ce0b
Adam Dubiel <adam.dubiel@allegrogroup.com>
2017-10-21 15:25:28 +0200

    Decouples Metrics creation from ZookeeperCounterReporter

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/di/factories/MetricRegistryFactory.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/di/factories/MetricRegistryFactory.java
@@ -14,8 +14,6 @@ import org.glassfish.hk2.api.Factory;
 import pl.allegro.tech.hermes.common.config.ConfigFactory;
 import pl.allegro.tech.hermes.common.config.Configs;
 import pl.allegro.tech.hermes.common.metric.HermesMetrics;
-import pl.allegro.tech.hermes.common.metric.counter.CounterStorage;
-import pl.allegro.tech.hermes.common.metric.counter.zookeeper.ZookeeperCounterReporter;
 import pl.allegro.tech.hermes.common.util.HostnameResolver;
 
 import javax.inject.Inject;
@@ -27,17 +25,14 @@ import java.util.concurrent.TimeUnit;
 public class MetricRegistryFactory implements Factory<MetricRegistry> {
 
     private final ConfigFactory configFactory;
-    private final CounterStorage counterStorage;
     private final HostnameResolver hostnameResolver;
     private final String moduleName;
 
     @Inject
     public MetricRegistryFactory(ConfigFactory configFactory,
-                                 CounterStorage counterStorage,
                                  HostnameResolver hostnameResolver,
                                  @Named("moduleName") String moduleName) {
         this.configFactory = configFactory;
-        this.counterStorage = counterStorage;
         this.hostnameResolver = hostnameResolver;
         this.moduleName = moduleName;
 
@@ -65,13 +60,6 @@ public class MetricRegistryFactory implements Factory<MetricRegistry> {
         if (configFactory.getBooleanProperty(Configs.METRICS_CONSOLE_REPORTER)) {
             ConsoleReporter.forRegistry(registry).build().start(
                     configFactory.getIntProperty(Configs.REPORT_PERIOD), TimeUnit.SECONDS
-            );
-        }
-
-        if (configFactory.getBooleanProperty(Configs.METRICS_ZOOKEEPER_REPORTER)) {
-            new ZookeeperCounterReporter(registry, counterStorage, configFactory).start(
-                    configFactory.getIntProperty(Configs.REPORT_PERIOD),
-                    TimeUnit.SECONDS
             );
         }
 
---
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/di/factories/ZookeeperMetricsFactory.java
@@ -0,0 +1,62 @@
+package pl.allegro.tech.hermes.common.di.factories;
+
+import com.codahale.metrics.MetricRegistry;
+import org.glassfish.hk2.api.Factory;
+import pl.allegro.tech.hermes.common.config.ConfigFactory;
+import pl.allegro.tech.hermes.common.config.Configs;
+import pl.allegro.tech.hermes.common.metric.counter.CounterStorage;
+import pl.allegro.tech.hermes.common.metric.counter.zookeeper.ZookeeperCounterReporter;
+import pl.allegro.tech.hermes.common.metric.counter.zookeeper.ZookeeperCounterStorage;
+import pl.allegro.tech.hermes.domain.subscription.SubscriptionRepository;
+import pl.allegro.tech.hermes.infrastructure.zookeeper.counter.DistributedEphemeralCounter;
+import pl.allegro.tech.hermes.infrastructure.zookeeper.counter.SharedCounter;
+import pl.allegro.tech.hermes.metrics.PathsCompiler;
+
+import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
+
+public class ZookeeperMetricsFactory implements Factory<CounterStorage> {
+
+    private final SharedCounter sharedCounter;
+    private final DistributedEphemeralCounter distributedCounter;
+    private final SubscriptionRepository subscriptionRepository;
+    private final PathsCompiler pathsCompiler;
+    private final ConfigFactory configFactory;
+    private final MetricRegistry metricRegistry;
+
+    private ZookeeperCounterReporter reporter;
+
+    @Inject
+    public ZookeeperMetricsFactory(SharedCounter sharedCounter, DistributedEphemeralCounter distributedCounter,
+                                   SubscriptionRepository subscriptionRepository,
+                                   PathsCompiler pathsCompiler, ConfigFactory configFactory, MetricRegistry metricRegistry) {
+        this.sharedCounter = sharedCounter;
+        this.distributedCounter = distributedCounter;
+        this.subscriptionRepository = subscriptionRepository;
+        this.pathsCompiler = pathsCompiler;
+        this.configFactory = configFactory;
+        this.metricRegistry = metricRegistry;
+    }
+
+    @Override
+    public CounterStorage provide() {
+        CounterStorage counterStorage = new ZookeeperCounterStorage(
+                sharedCounter, distributedCounter, subscriptionRepository, pathsCompiler, configFactory
+        );
+
+        if (configFactory.getBooleanProperty(Configs.METRICS_ZOOKEEPER_REPORTER)) {
+            reporter = new ZookeeperCounterReporter(metricRegistry, counterStorage, configFactory);
+            reporter.start(
+                    configFactory.getIntProperty(Configs.REPORT_PERIOD),
+                    TimeUnit.SECONDS
+            );
+        }
+
+        return counterStorage;
+    }
+
+    @Override
+    public void dispose(CounterStorage instance) {
+        reporter.stop();
+    }
+}
